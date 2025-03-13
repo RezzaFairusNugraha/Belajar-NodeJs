6931c5db038cf7f9994f2948dbddb7a15a7e135a
Rezza Fairus Nugraha <rezafairusfugraha@gmail.com>
2025-03-13 10:35:24 +0700

    memperbarui app.js and contact.js

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
-const {simpanContact,tulisPertanyaan,listContacts,deleteContact } = require('./contact.js')
+const {
+  simpanContact,
+  tulisPertanyaan,
+  listContacts,
+  deleteContact,
+  detail,
+} = require("./contact.js");
 
 console.log("Selamat datang di aplikasi ini");
 console.log("Pilihlah salah satu opsi dibawah ini");
 console.log("1. Tambahkan data");
 console.log("2. Hapus data");
 console.log("3. Tampilkan data");
+console.log("4. Detail data");
 
 async function main() {
-  const input = await tulisPertanyaan("Masukan Input dengan angka 1 - 3 : ");
-  if(input === "1"){
+  const input = await tulisPertanyaan("Masukan Input dengan angka 1 - 4 : ");
+  if (input === "1") {
     console.log("Kamu memilih angka 1 berarti kamu akan menambahkan data");
     const nama = await tulisPertanyaan("Masukkan nama anda : ");
     const email = await tulisPertanyaan("Masukkan email anda : ");
     const phone = await tulisPertanyaan("Masukkan no telepon anda : ");
-    simpanContact(nama,email,phone)
-  }else if(input === "2"){
+    simpanContact(nama, email, phone);
+  } else if (input === "2") {
     console.log("Kamu memilih angka 2 berarti kamu akan menghapus data");
     deleteContact();
-  }else if(input === "3"){
+  } else if (input === "3") {
     console.log("Kamu memilih angka 3 berarti kamu akan menampilkan data");
     listContacts();
-  }else{
-    console.log("Kamu tidak memilih angka 1 - 3");
-    
+  } else if (input === "4") {
+    console.log("Kamu memilih angka 4 berarti kamu akan detail data");
+    detail();
+  } else {
+    console.log("Kamu tidak memilih angka 1 - 4");
   }
 }
 
 main();
-
